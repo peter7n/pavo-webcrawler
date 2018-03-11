7ba8e8f76dae7396a95622c0c15646d1e3ae341f
Peter Nguyen <ptn045@ucsd.edu>
2018-03-10 17:29:32 -0800

    keyword found: node and also tooltip now displays red; will also work for node ID-1 comments/documentation added to visualizer js and css files fix issue with errMsgTxt messing up visualizer (remove margin-bottom)

--- website files/js/visualizer.js
+++ website files/js/visualizer.js
@@ -1,8 +1,22 @@
+/******************************************************************************
+* Name:        visualizer.js
+* Author:      Peter Nguyen
+* Created:     3/1/18
+* Description: JavaScript that generates the visual output for Team Pavo's
+*              graphical web crawler
+******************************************************************************/
+
 var ns = "http://www.w3.org/2000/svg";
 var nodeCounter = 0;
 
+/******************************************************************************
+* Function:    calcSvgSize
+* Parameters:  svg, nodeCount
+* Returns:     nothing
+* Description: Dynamically calculates the maximum width and height of the svg
+*              container
+******************************************************************************/
 function calcSvgSize(svg, nodeCount) {
-	// Calculate maximum svg width and height
 	var maxLeft = 0;
 	var maxTop = 0;
 	for (var i = 1; i < nodeCount; i++) {
@@ -17,13 +31,23 @@ function calcSvgSize(svg, nodeCount) {
 	svg.setAttributeNS(null, "height", maxTop);
 }
 
+/******************************************************************************
+* Function:    drawLines
+* Parameters:  webcrawlResults, svg, nodeCount
+* Returns:     nothing
+* Description: Draws the connecting lines between link nodes using svg
+*              graphics
+******************************************************************************/
 function drawLines(webcrawlResults, svg, nodeCount) {
+	// Get heights of various divs to correctly offset the y coordinate
 	var infoDivHeight = parseInt($("#inputInformation").css("height"));
 	var headerHeight = parseInt($("#crawler-header").css("height"));
 	var errorDivHeight = 0;
 	if ($("#errorMessage").css("display") != "none")
 		errorDivHeight = parseInt($("#errorMessage").css("height"));
 
+	// Loop through the nodes, get the coordinates of the node and its parent,
+	// and draw a line between these coordinates
 	for (var j = 1; j < nodeCount; j++) {
 		var nodeId = "#ID-" + (j + 1);
 		var urlNode = $(nodeId);
@@ -31,53 +55,80 @@ function drawLines(webcrawlResults, svg, nodeCount) {
 		var parent = $(urlNode.parent());
 		var parentOffset = parent.offset();
 		var line = document.createElementNS(ns, "line");
-        line.setAttributeNS(null, "x1", offset.left);
-        line.setAttributeNS(null, "y1", offset.top - infoDivHeight - headerHeight - errorDivHeight);
-        line.setAttributeNS(null, "x2", parentOffset.left);
-        line.setAttributeNS(null, "y2", parentOffset.top - infoDivHeight - headerHeight - errorDivHeight);
+		line.setAttributeNS(null, "x1", offset.left);
+		line.setAttributeNS(null, "y1", offset.top - infoDivHeight - headerHeight - errorDivHeight);
+		line.setAttributeNS(null, "x2", parentOffset.left);
+		line.setAttributeNS(null, "y2", parentOffset.top - infoDivHeight - headerHeight - errorDivHeight);
 		line.setAttributeNS(null, "stroke", "#4483d5");
 		svg.appendChild(line);
-
-		// set the node where keyword was found to red
-		if (nodeId == "#" + webcrawlResults.keywordWebsite) {
-			urlNode.find(".inner").css({"background":"red"});
-		}
 	}
 }
 
+/******************************************************************************
+* Function:    removeLines
+* Parameters:  svg
+* Returns:     nothing
+* Description: Deletes all existing lines from the svg element
+******************************************************************************/
 function removeLines(svg) {
 	while (svg.firstChild) {
 		svg.removeChild(svg.firstChild);
 	}
 }
 
-// Recursive function that traverses json tree and
-// appends node elements to the specified container
-function jsonTreeRecurse(container, jsonData) {
+/******************************************************************************
+* Function:    jsonTreeRecurse
+* Parameters:  container, jsonData, keywordId
+* Returns:     nothing
+* Description: Recursive function that traverses JSON tree and appends node
+*              elements in the DOM to the specified container. Populates
+*              the node with the website's title, URL and anchor link
+******************************************************************************/
+function jsonTreeRecurse(container, jsonData, keywordId) {
 	 for (var i = 0; i < jsonData.length; i++) {
-		  var $divParent = $("<div class='outer'></div>");
-		  var $divChild = $("<a target='_blank'></a>");
-		  var $divtooltipcont = $("<div class='tooltipcont tooltip-hidden'></div>");
-		  var $divInner=$("<div class='inner'></div>");
-		  var $divSpan = $("<span class='tooltiptext'></span>");
-		  $divParent.attr('id', jsonData[i].id);
-		  $divChild.attr('href', jsonData[i].url);
-		  $divSpan.text(jsonData[i].title + " | " + jsonData[i].url);
-		  $divInner.append($divSpan);
-		  $divtooltipcont.append($divInner);
-		  $divChild.append($divtooltipcont);
-		  $divParent.append($divChild);
-		  if (jsonData[i].destinations) {
-			  jsonTreeRecurse($divParent, jsonData[i].destinations);
+		  var divParent = $("<div class='outer'></div>");
+		  var divChild = $("<a target='_blank'></a>");
+		  var divtooltipcont = $("<div class='tooltipcont tooltip-hidden'></div>");
+		  var divSpan = $("<span class='tooltiptext'></span>");
+		  var divInner = $("<div class='inner'></div>");
+
+		  // If keyword is found, mark this node red
+		  if (jsonData[i].id == keywordId) {
+			  divInner = $("<div class='inner' style='background: red;'></div>");
+			  divSpan = $("<span class='tooltiptext' style='background: red;'></span>");
+			  divSpan.text("* KEYWORD FOUND * " + jsonData[i].title + " | " + jsonData[i].url);
 		  }
-		  container.append($divParent);
+		  // Give the node an id and the website's title and URL
+		  else {
+			  divSpan.text(jsonData[i].title + " | " + jsonData[i].url);
+		  }
+		  divParent.attr('id', jsonData[i].id);
+		  divChild.attr('href', jsonData[i].url);
+		  divInner.append(divSpan);
+		  divtooltipcont.append(divInner);
+		  divChild.append(divtooltipcont);
+		  divParent.append(divChild);
+
+		  // If node has more destinations, call function recursively
+		  if (jsonData[i].destinations) {
+			  jsonTreeRecurse(divParent, jsonData[i].destinations, keywordId);
+		  }
+		  container.append(divParent);
 		  nodeCounter++;
 	 }
 }
 
+/******************************************************************************
+* Function:    displayVisualizer
+* Parameters:  webcrawlResults
+* Returns:     nothing
+* Description: Main visualizer function. Generates the visual display as well
+*              as provides control for the zoom function and toggle to
+*              show/hide all labels.
+******************************************************************************/
 function displayVisualizer(webcrawlResults) {
 	 // Create the node elements
-	 jsonTreeRecurse($("#overlay"), webcrawlResults.websites);
+	 jsonTreeRecurse($("#overlay"), webcrawlResults.websites, webcrawlResults.keywordWebsite);
 
 	 // Create svg element
 	 var div = document.getElementById("drawing");
@@ -85,13 +136,13 @@ function displayVisualizer(webcrawlResults) {
 	 calcSvgSize(svg, nodeCounter);
 	 div.appendChild(svg);
 
-	 // Draw connecting lines between parent and child nodes
+	 // Draw connecting lines between the website nodes
 	 drawLines(webcrawlResults, svg, nodeCounter);
 
-	 // Initialize the slider and switch controls
 	 $("#control").show();
 	 var slider = document.getElementById("slideRange");
 
+	 // Show and hide all labels if switch is toggled
 	 $('input[type=checkbox]').change(function() {
 		 if (this.checked) {
 			 $(".tooltipcont").toggleClass("tooltip-hidden");
@@ -103,14 +154,12 @@ function displayVisualizer(webcrawlResults) {
 		 }
 	 });
 
-	 // Update the current slider value
+	 // Dynamically shrink/expand the visualizer graphic based on slider input
 	 slider.oninput = function() {
-		 // output.innerHTML = this.value;
 		 removeLines(svg);
 		 var leftVal = Math.round(this.value);
 		 var marginVal = Math.round(this.value * .015);
 		 var topVal = Math.round(this.value * .05);
-		 console.log(this.value);
 		 $(".outer").css("left", leftVal);
 		 $(".outer").css("margin-bottom", marginVal);
 		 $(".outer").css("top", topVal);
